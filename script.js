65244703f20d07912d297e4631c525ef9f695c8a
Demontier <demontier@outlook.com>
2021-06-16 16:24:53 -0300

    requisitos 1,2,3,4,5 e 6

---
+++ script.js
@@ -0,0 +1,14 @@
+const senha = document.querySelector('.password');
+const login = document.querySelector('.login');
+const entrar = document.querySelector('.buttonLogin');
+
+function alerta() {
+  entrar.addEventListener('click', () => {
+    if(login.value === 'tryber@teste.com' && senha.value === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Login ou senha inválidos.');
+    }
+  });
+}
+alerta()
