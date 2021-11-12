e1b62a6ff9adf62c80ff783497832dc04d529398
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-11-12 09:21:14 -0300

    requisito 21

--- script.js
+++ script.js
@@ -47,8 +47,67 @@ textArea.addEventListener('change', () => {
 });
 
 const submitbtn = document.querySelector('#submit-btn');
-const main = document.querySelector('.main');
+const form = ['Email:', 'Casa:', 'Família:', 'Matérias:', 'Avaliação:', 'Observações:'];
+const subjects = document.querySelectorAll('.contents');
+const evaluator = document.querySelectorAll('.evaluator');
+const family = document.querySelectorAll('.containerFamily');
+const numbersSubjects = [];
+
+function checkFamily() {
+  for (let index = 0; index < 3; index += 1) {
+    if (family[index].checked) {
+      sessionStorage.setItem('form4', family[index].value);
+    }
+  }
+}
+
+function checkStudies() {
+  for (let index = 0; index < 6; index += 1) {
+    if (subjects[index].checked) {
+      numbersSubjects.push(` ${subjects[index].value}`);
+    }
+  }
+  sessionStorage.setItem('form5', numbersSubjects);
+}
+
+function checkEvaluator() {
+  for (let index = 0; index < 10; index += 1) {
+    if (evaluator[index].checked) {
+      sessionStorage.setItem('form6', evaluator[index].value);
+    }
+  }
+}
+
+submitbtn.addEventListener('click', () => {
+  const containerMain = document.querySelectorAll('.containerMain');
+  const house = document.querySelector('#house');
+  sessionStorage.setItem('form0', containerMain[0].value);
+  sessionStorage.setItem('form1', containerMain[1].value);
+  sessionStorage.setItem('form2', containerMain[2].value);
+  sessionStorage.setItem('form7', containerMain[3].value);
+  sessionStorage.setItem('form3', house.value);
+  checkFamily();
+  checkStudies();
+  checkEvaluator();
+});
+
+const main = document.querySelector('#evaluation-form');
 submitbtn.addEventListener('click', (event) => {
   event.preventDefault();
   main.innerHTML = '';
+});
+
+submitbtn.addEventListener('click', () => {
+  const name = sessionStorage.getItem(`form${0}`);
+  const lastName = sessionStorage.getItem(`form${1}`);
+  const mainForm = document.querySelector('#evaluation-form');
+  const fullName = document.createElement('p');
+  fullName.innerText = `Nome: ${name} ${lastName}`;
+  mainForm.appendChild(fullName);
+
+  for (let index = 0; index < form.length; index += 1) {
+    const paragraphName = document.createElement('p');
+    paragraphName.innerText = `${form[index]} ${sessionStorage.getItem(`form${index + 2}`)}`;
+    mainForm.appendChild(paragraphName);
+  }
 });
